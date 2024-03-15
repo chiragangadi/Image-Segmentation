679deb614acbfc9a50d0664a9f4378339fe5fad3
chiragangadi <chirag.s.angadi1408@gmail.com>
2024-03-15 23:46:25 +0100

    Add untracked files

---
+++ ConvexNet.py
@@ -0,0 +1,22 @@
+# Convex architecture neural network
+class NetConvex(nn.Module):
+    def __init__(self):
+        super(NetConvex, self).__init__()
+        self.fc1 = nn.Linear(2,256)
+        self.fc2 = nn.Linear(256,256)
+        self.fc3 = nn.Linear(256,1)
+        self.skip1 = nn.Linear(2,256)
+        self.skip2 = nn.Linear(2,1)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = x
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x) + self.skip1(y)
+        x = self.relu(x)
+        x = self.fc3(x) + self.skip2(y)
+        x = self.sigmoid(x)
+        
+        return x
---
+++ FCNN.py
@@ -0,0 +1,19 @@
+# Fully connected neural network
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(5, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 1)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x) 
+        x = self.relu(x)
+        x = self.fc3(x) 
+        x = self.sigmoid(x)
+        
+        return x
